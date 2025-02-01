dfc854f0dfe1bd5f94d5bee2afe74e6423ad9b60
sergey200519 <oborok05@bk.ru>
2025-02-01 22:25:14 +0300

    global reset

--- src/modules/UserCode/html/UserCodeHtml.ts
+++
@@ -1,9 +0,0 @@
-import { UserCodeEditorContext } from "../../types/types";
-import { UserCode } from "../UserCode";
-
-export class UserCodeHtml extends UserCode {
-    constructor(userCodeBox: HTMLElement, context: UserCodeEditorContext) {
-        super(userCodeBox, context);
-        console.log("html");
-    }
-}
